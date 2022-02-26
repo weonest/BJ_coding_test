570a62ac8235ed50799dedaf9004e6ae4f8a1841
weonest <geonhee33@naver.com>
2022-02-26 16:41:30 +0900

    Study

---
+++ src/PickNumberUnder5.java
@@ -0,0 +1,54 @@
+
+import java.util.Scanner;
+
+public class PickNumberUnder5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 내 풀이
+        int N = sc.nextInt();
+        int X = sc.nextInt();
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+
+        for (int j = 0; j < N; j++) {
+            if (arr[j] < X) {
+                System.out.print(arr[j] + " ");
+            }
+        }
+
+        // 버퍼드리퍼 풀이 (난이도 높음)
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.StringTokenizer;
+//
+//        public class Main {
+//            public static void main(String[] args) throws IOException {
+//                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//                StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+//
+//                int N = Integer.parseInt(st.nextToken());
+//                int X = Integer.parseInt(st.nextToken());
+//
+//                StringBuilder sb = new StringBuilder();
+//
+//                st = new StringTokenizer(br.readLine(), " ");
+//
+//                for (int i = 1; i < N; i++) {
+//                    int value = Integer.parseInt(st.nextToken());
+//
+//                    if (value < X) {
+//                        sb.append(value).append(' ');
+//                    }
+//                    System.out.println(sb);
+//                }
+//            }
+//        }
+    }
+}
--- src/ReverseStar.java
+++ src/ReverseStar.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class Main {
+public class ReverseStar {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
