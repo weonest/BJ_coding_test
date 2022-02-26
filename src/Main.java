69c0a50465321c0f25c38e7d638a49821f5f6a03
weonest <geonhee33@naver.com>
2022-02-26 16:03:04 +0900

    Study

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,25 @@
-// 알람 시계
-// 내가 작성한 식과 코드의 간결성의 차이를 확인할 것!
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        int x = sc.nextInt();
-        int y = sc.nextInt();
-        
-        /* 내가 짠 코드
-        if (x > 0 && y < 45 ){
-            y = y + 15;
-            x = x - 1;
-            System.out.println(x + " " + y);
-        } else if (y > 45){
-            y = y - 45;
-            System.out.println(x + " " + y);
-        } else{
-            y = y + 15;
-            x = 23;
-            System.out.println(x + " " + y);
-        }*/
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        if (y < 45) {
-            x--; //시간 감소
-            y = 60 - (45 - y); // 분 감소
-            if (x < 0) {
-                x = 23;
-            }
-            System.out.println(x + " " + y);
+        int dice1 = Integer.parseInt(st.nextToken());
+        int dice2 = Integer.parseInt(st.nextToken());
+        int dice3 = Integer.parseInt(st.nextToken());
+
+        if (dice1 == dice2 && dice1 == dice3) {
+            System.out.println(10000 + (dice1 * 1000));
+        } else if (dice1 == dice2 || dice1 == dice3) {
+            System.out.println(1000 + (dice1 * 100));
+        } else if (dice2 == dice3) {
+            System.out.println(1000 + (dice2 * 100));
         } else {
-            System.out.println(x + " " + (y - 45));
+            System.out.println(Math.max(Math.max(dice1, dice2),dice3) * 100);
         }
     }
 }
---
+++ src/TimeCal.java
@@ -0,0 +1,37 @@
+// 알람 시계
+// 내가 작성한 식과 코드의 간결성의 차이를 확인할 것!
+import java.util.Scanner;
+
+public class TimeCal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        
+        /* 내가 짠 코드
+        if (x > 0 && y < 45 ){
+            y = y + 15;
+            x = x - 1;
+            System.out.println(x + " " + y);
+        } else if (y > 45){
+            y = y - 45;
+            System.out.println(x + " " + y);
+        } else{
+            y = y + 15;
+            x = 23;
+            System.out.println(x + " " + y);
+        }*/
+
+        if (y < 45) {
+            x--; //시간 감소
+            y = 60 - (45 - y); // 분 감소
+            if (x < 0) {
+                x = 23;
+            }
+            System.out.println(x + " " + y);
+        } else {
+            System.out.println(x + " " + (y - 45));
+        }
+    }
+}
