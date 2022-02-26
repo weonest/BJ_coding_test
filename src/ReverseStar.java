657715abc8ac204f98c419c26db0079f33afaeb5
weonest <geonhee33@naver.com>
2022-02-26 16:28:56 +0900

    Study

---
+++ src/ReverseStar.java
@@ -0,0 +1,34 @@
+// 역순 별찍기
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+//        for (int i = 0; i < a; i++) {
+//            for (int j = 0; j < a; j++) {
+//                if (i + j < a - 1) {
+//                    System.out.print(" ");
+//                } else {
+//                    System.out.print("*");
+//                }
+//            }
+//            System.out.println(" ");
+//        }
+
+
+//        for (int i = 1; i <= a; i++) {
+//            for (int j = a; j > 0; j--) {
+//                if (i < j) {
+//                    System.out.print(" ");
+//                } else {
+//                    System.out.print("*");
+//                }
+//            }
+//            System.out.println(" ");
+//        }
+    }
+}
