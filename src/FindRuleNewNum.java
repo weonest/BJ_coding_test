378047424b6d0ea341e9e1b0e51c5b829c0904a9
weonest <geonhee33@naver.com>
2022-02-27 16:31:17 +0900

    Study

---
+++ src/FindRuleNewNum.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class FindRuleNewNum {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int fir = sc.nextInt();
+        sc.close();
+
+        int result = (10 * (fir % 10)) + ((fir / 10 + fir % 10) % 10);
+        int count = 1;
+
+//        int T = fir / 10; // 2
+//        int N = fir % 10; // 6
+//        int sum  = tn + n; // 8
+//        int new = 10 * n + sum % 10; // 68
+
+        while (fir != result) {
+            result = (10 * (result % 10)) + ((result / 10 + result % 10) % 10);
+            count++;
+            if (fir == result) {
+                break;
+            }
+        }
+        System.out.println(count);
+    }
+}
