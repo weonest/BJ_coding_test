ce6cc756ec266687b30a82029bd6069f122c331e
Weon-geonhee <geonhee33@naver.com>
2022-03-01 15:03:33 +0900

    Study

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,32 @@ public class Main {
         // 숫자 2개를 입력 받고 크기를 비교하는 코드
         Scanner sc = new Scanner(System.in);
 
-        int a, b;
+//        int a, b;
+//
+//        a = sc.nextInt();
+//        b = sc.nextInt();
+//
+//        System.out.println("둘 중에 큰 수는: ");
+//
+//        System.out.print(a > b ? a : (a == b ? "같음" : b));
+//                        // a가 b보다 크면 'a' 출력, 아니면 다시 (a와 b가 같으면 '같음' 출력 아니면 b
+//
 
-        a = sc.nextInt();
-        b = sc.nextInt();
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-        System.out.println("둘 중에 큰 수는: ");
+        boolean flag = true;
 
-        System.out.print(a > b ? a : (a == b ? "같음" : b));
-                        // a가 b보다 크면 'a' 출력, 아니면 다시 (a와 b가 같으면 '같음' 출력 아니면 b
+        while (flag) {
+            try {
+                System.out.print("숫자를 입력하세요 : ");
+                int i = sc.nextInt();
+                System.out.println(arr[i-1]); // 인덱스 값과 저장된 수의 값의 차가 1이므로
+                System.out.println("프로그램 종료");
+                break;
+            } catch (Exception e) {
+                System.out.println("1 ~ 10 사이의 값을 입력하세요");
+
+            }
+        }
     }
 }
