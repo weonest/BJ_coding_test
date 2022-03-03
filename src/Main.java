98bd457383ac5e30edda93b24c2e0150d21662c5
weonest <geonhee33@naver.com>
2022-03-03 23:33:16 +0900

    "Baekjoon - 2562"

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -5,23 +7,27 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] arr = new int[5];
+        int[] arr = new int[9];
 
         int temp = 0;
 
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
-        for (int j = 0; j < arr.length; j++) {
-            for (int k = 0; k < arr.length; k++) {
-                if (arr[j] < arr[k]) {
-                    temp = arr[j];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] < arr[j]) {
+                    temp = arr[i];
                     System.out.println("temp : " + temp);
-                    arr[j] = arr[k];
-                    System.out.println("arr[j] : " + arr[j]);
-                    arr[k] = temp;
-                    System.out.println("arr[k] : " + arr[k]);
-                    System.out.println(arr[0] + " " + arr[1] + " " + arr[2] + " " + arr[3] + " " + arr[4]);
+                    arr[i] = arr[j];
+                    System.out.printf("arr[i = %d] : ",i);
+                    System.out.println(arr[i]);
+                    arr[j] = temp;
+                    System.out.printf("arr[j = %d] : ",j);
+                    System.out.println(arr[j]);
+                    System.out.println(arr[0] + " " + arr[1] + " " + arr[2] + " " + arr[3] + " " + arr[4] + " " + arr[5] + " " + arr[6] + " " + arr[7] + " " + arr[8]);
+                    System.out.println(" ");
+                    // 3 29 38 12 57 74 40 85 61
                 }
             }
         }
