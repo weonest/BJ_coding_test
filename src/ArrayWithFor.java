669c6fbe8d2f496dd45ea79870c631be20dc8e11
weonest <geonhee33@naver.com>
2022-03-04 15:49:50 +0900

    "Baekjoon - 2562"

--- src/ArrayWithFor.java
+++ src/ArrayWithFor.java
@@ -1,6 +1,5 @@
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+// 삽 입 정 렬
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayWithFor {
@@ -18,17 +17,17 @@ public class ArrayWithFor {
             for (int j = 0; j < arr.length; j++) {
                 if (arr[i] < arr[j]) {
                     temp = arr[i]; // temp값에 [i]값 저장      스와핑
-                    System.out.println("temp : " + temp);
+//                 #1 System.out.printf("temp = arr[%d] : %d\n", i, temp);
 
                     arr[i] = arr[j]; // [i]값에 [j]값 저장       스와핑
-                    System.out.printf("arr[i = %d] : ",i);
-                    System.out.println(arr[i]);
+//                 #2 System.out.printf("arr[i = %d] : ", i);
+//                    System.out.println(arr[i]);
                     arr[j] = temp; // [j]값에 temp값 저장        스와핑
-                    System.out.printf("arr[j = %d] : ",j);
-                    System.out.println(arr[j]);
+//                 #3 System.out.printf("arr[j = %d] : ", j);
+//                    System.out.println(arr[j]);
 
-                    System.out.println(arr[0] + " " + arr[1] + " " + arr[2] + " " + arr[3] + " " + arr[4] + " " + arr[5] + " " + arr[6] + " " + arr[7] + " " + arr[8]);
-                    System.out.println(" "); // 정렬된 배열 출력하기
+//                 #4 System.out.println(arr[0] + " " + arr[1] + " " + arr[2] + " " + arr[3] + " " + arr[4] + " " + arr[5] + " " + arr[6] + " " + arr[7] + " " + arr[8]);
+//                    System.out.println(" "); // 정렬된 배열 출력하기
                     // 3 29 38 12 57 74 40 85 61
                 }
             }
--- src/ArrayWithFor_2562.java
+++ src/ArrayWithFor_2562.java
@@ -1,32 +1,33 @@
 import java.util.Scanner;
 
-    public class ArrayWithFor_2562 {
-        public static void main(String[] args) {
+public class ArrayWithFor_2562 {
+    public static void main(String[] args) {
 
-            Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-            int[] arr = new int[9];
+        int[] arr = new int[9];
 
-            int index = 0;
-            int max = 0;
+        int index = 0;
+        int max = 0;
 
-            for (int i = 0; i < arr.length; i++) {
-                arr[i] = sc.nextInt();
-            }
-            sc.close();
-
-            for (int j = 0; j < arr.length; j++) {
-                if (max < arr[j]) {
-                    max = arr[j]; // 최대값으로 지정해서 저장
-//                    System.out.printf("max = arr[%d] = %d\n", j, arr[j]);
-
-                    index = j; // 저장된 최소값을 바탕으로 몇번째 인덱스인지 저장
-//                    System.out.printf("index = %d\n", j);
-//                    System.out.println(arr[0] + " " + arr[1] + " " + arr[2] + " " + arr[3] + " " + arr[4] + " " + arr[5] + " " + arr[6] + " " + arr[7] + " " + arr[8]);
-//                    System.out.println(" "); // 정렬된 배열 출력하기
-                }
-            }
-            System.out.println(arr[index]);
-            System.out.println(index + 1);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
         }
+        sc.close();
+
+        for (int j = 0; j < arr.length; j++) {
+            if (max < arr[j]) {
+                max = arr[j]; // 최대값으로 지정해서 저장
+//                  System.out.printf("max = arr[%d] = %d\n", j, arr[j]);
+
+                index = j; // 저장된 최소값을 바탕으로 몇번째 인덱스인지 저장
+//                  System.out.printf("index = %d\n", j);
+//                  System.out.println(arr[0] + " " + arr[1] + " " + arr[2] + " " + arr[3] + " " + arr[4] + " " + arr[5] + " " + arr[6] + " " + arr[7] + " " + arr[8]);
+//                  System.out.println(" "); // 정렬된 배열 출력하기
+//                3 29 38 12 57 74 40 85 61
+            }
+        }
+        System.out.println(arr[index]);
+        System.out.println(index + 1);
     }
+}
